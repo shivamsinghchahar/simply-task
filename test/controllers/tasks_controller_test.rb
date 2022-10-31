311a3fe50500d1d9317bfaec69a24ec5aa3e958a
Shivam Chahar <chahar.shivam@yahoo.com>
2022-10-31 12:02:49 +0530

    Added edit, update actions and edit form

--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -3,10 +3,11 @@
 require "test_helper"
 
 class TasksControllerTest < ActionDispatch::IntegrationTest
-  attr_reader :user
+  attr_reader :user, :task
 
   def setup
     @user = create :user
+    @task = create :task, user:
   end
 
   def test_should_get_new
@@ -48,6 +49,48 @@ class TasksControllerTest < ActionDispatch::IntegrationTest
     post tasks_path, params: { task: attributes_for(:task).except(:user_id) }
 
     assert_equal "create", @controller.action_name
+    assert_redirected_to new_session_path
+  end
+
+  def test_should_get_edit
+    login user
+    get edit_task_path(task)
+
+    assert_equal "edit", @controller.action_name
+    assert_response :success
+  end
+
+  def test_should_redirect_edit_if_not_logged_in
+    get edit_task_path(task)
+
+    assert_equal "edit", @controller.action_name
+    assert_redirected_to new_session_path
+  end
+
+  def test_should_update_task_with_valid_attributes
+    login user
+    assert_changes 'task.reload.name' do
+      put task_path(task), params: { task: { name: "New name" } }
+    end
+
+    assert_equal "update", @controller.action_name
+    assert_equal "Successfully updated task", flash[:notice]
+  end
+
+  def test_should_not_update_task_with_invalid_attributes
+    login user
+    assert_no_changes 'task.reload.name' do
+      put task_path(task), params: { task: { name: "" } }
+    end
+
+    assert_equal "update", @controller.action_name
+    assert_response :unprocessable_entity
+  end
+
+  def test_should_redirect_update_if_not_logged_in
+    put task_path(task), params: { task: attributes_for(:task).except(:user_id) }
+
+    assert_equal "update", @controller.action_name
     assert_redirected_to new_session_path
   end
 end

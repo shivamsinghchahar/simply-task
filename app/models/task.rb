ea1f15d582cddb1065888302ee36d5db0aebe605
Shivam Chahar <chahar.shivam@yahoo.com>
2022-11-01 12:36:09 +0530

    Add overdue and completed scopes

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,4 +4,7 @@ class Task < ApplicationRecord
   belongs_to :user
 
   validates :name, :due_date, presence: true
+
+  scope :completed, -> { where(is_completed: true) }
+  scope :overdue,   -> { where(due_date: ..Date.current, is_completed: false) }
 end
